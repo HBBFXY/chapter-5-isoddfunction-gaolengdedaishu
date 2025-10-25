572090ed006fa1eec0d3b25a62e269ce4af92ecd
gaolengdedaishu <1942806425@qq.com>
2025-10-25 17:04:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ def isOdd(value):
     返回:
     bool - 如果是整数且为奇数返回 True，否则返回 False
     """
+
+    if isinstance(value, bool):
+        return False
+    if isinstance(value, int):
+        if value % 2 != 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+if __name__ == "__main__":
+    N = input()
+    num = isOdd(N)
+    print(num)
     # 学生实现代码区域
     # 提示：首先检查类型是否为整数，然后检查奇偶性
     
